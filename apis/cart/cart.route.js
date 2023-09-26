2ec0c57f6d9e2e928fa9edb3d00836b1981a8f42
nirav <patilnirav70@gmail.com>
2023-09-26 17:02:52 +0530

    added1

--- apis/cart/cart.route.js
+++ apis/cart/cart.route.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const CartController = require('./cart.controller');
+const authh = require('../middlewares/authh');
+const roleAccess = require("../middlewares/roleAccess");
 
-router.get('/:userId', CartController.getCart);
-router.post('/:userId', CartController.addItemToCart);
-router.delete("/:userId/:productId", CartController.removeItem);
+router.get('/:userId',  authh, CartController.getCart);
+router.post('/:userId',  authh , CartController.addItemToCart);
+router.delete("/:userId/:productId", authh, CartController.removeItem);
 
 
 module.exports = router;
